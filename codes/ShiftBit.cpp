e7b7442628f310acdbf7cc5b039fea2bdee492b5
LatteEater <tanitpong.wat@mail.pbru.ac.th>
2017-03-09 18:48:04 +0700

    complete

--- codes/ShiftBit.cpp
+++ codes/ShiftBit.cpp
@@ -1,5 +1,15 @@
-  #include <iostream.h>
-  using namespace std;
-  main(){
-  
-  }
+#include <iostream.h>
+using namespace std;
+
+void testbit(int d) {
+  int x = (1<<2);
+  int a = (d&x);
+  cout << "Test Shift : " << d<<endl;
+  cout << "Test bit 3 = 1 is : " << ((d&x)==x);
+  (a==1) ? (cout<<"odd"):(cout<<"even");
+}
+void main(){
+
+  testbit(10);
+
+}
