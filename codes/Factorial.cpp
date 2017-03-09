5affb4a6a9b5ad0349005bcbf93478da8ba49ba8
LatteEater <tanitpong.wat@mail.pbru.ac.th>
2017-03-09 19:06:54 +0700

    Factorial Complete

--- codes/Factorial.cpp
+++ codes/Factorial.cpp
@@ -1,7 +1,14 @@
 #include <iostream.h>
 using namespace std;
-void main(){
+int fac(int d){
+    int i,r=1;
+    for (int j = 1; j <=d ; j++) {
+        r = r*i;
+    }
+    return r;
+}
 
-    
+void main(){   
+    cout<<fac(5);
 
 }
